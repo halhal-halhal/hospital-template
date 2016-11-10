9060300aa895969374afb38f421197dfba664c55
halspring <hal.webworks@gmail.com>
2016-11-10 17:46:27 +0900

    page.html を dormitory.html に名前変更

---
+++ table.php
@@ -0,0 +1,80 @@
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名1' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容1' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名2' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容2' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名3' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容3' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名4' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容4' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名5' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容5' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名6' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容6' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名7' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容7' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名8' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容8' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名9' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容9' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名10' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容10' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名11' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容11' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名12' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容12' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名13' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容13' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名14' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容14' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名15' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容15' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名16' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容16' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名17' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容17' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名18' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容18' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名19' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容19' ,true); ?></th>
+</tr>
+<tr>
+  <td><?php echo get_post_meta($post->ID, '項目名20' ,true)?></td>
+  <th><?php echo get_post_meta($post->ID, '内容20' ,true); ?></th>
+</tr>
